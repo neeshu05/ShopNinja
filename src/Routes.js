e475d66867c8c272a83ef7f5022ee5136a547886
neeshu maini <97976949+neeshu05@users.noreply.github.com>
2023-07-19 17:22:54 +0530

    Update Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Home from "./core/Home";
 import Signup from "./user/Signup";
-import PrivateRoutes from "./auth/helper/PrivateRoutes";
 import UserDashboard from "./user/UserDashboard";
 import Signin from "./user/Signin";
 import Cart from "./core/Cart";
@@ -16,7 +15,6 @@ const Routes = () => {
         <Route path="/signup" exact component={Signup} />
         <Route path="/signin" exact component={Signin} />
         <Route path="/cart" exact component={Cart} />
-        <PrivateRoutes path="/user/dashboard" exact component={UserDashboard} />
       </Switch>
     </BrowserRouter>
   );
